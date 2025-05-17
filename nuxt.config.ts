a6e2a8877faa48cc6082f5c9cb52a4025ca2a288
PunJakkapong <jakkapong@goship.co.th>
2025-05-17 21:28:36 +0700

    change title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,8 @@ export default defineNuxtConfig({
   css: ["bootstrap/dist/css/bootstrap.min.css"],
   app: {
     head: {
+      title: "Restaurants Map",
+      titleTemplate: "%s - Restaurants Map",
       script: [
         {
           src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
