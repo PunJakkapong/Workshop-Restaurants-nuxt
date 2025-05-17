9dd0c57ccb426e951502c9bb8daaf67fc4e0ed7d
PunJakkapong <jakkapong@goship.co.th>
2025-05-17 21:58:45 +0700

    change web icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,13 @@ export default defineNuxtConfig({
           crossorigin: "anonymous",
         },
       ],
+      link: [
+        {
+          rel: "icon",
+          type: "image/svg+xml",
+          href: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🍽️</text></svg>',
+        },
+      ],
     },
   },
   modules: [
